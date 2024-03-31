b39ff12a19fa60461f99b40cdf8c8881c0dae2ac
Ian Koech <ianovic8@live.com>
2024-03-31 20:24:27 +0300

    Message Card for Left Sidebar

---
+++ csm/app/components/MessageCard.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Avatar from "boring-avatars";
+
+export interface MessageInfo {
+  content: string;
+  displayName: string;
+  assigned: Boolean;
+}
+
+function MessageCard(props: MessageInfo) {
+  return (
+    <div className=" p-4 w-full self-end flex items-center hover:bg-[#2b2d31] rounded-xl hover:cursor-pointer mb-3">
+      <div className="mr-5">
+        <Avatar
+          size={52}
+          name={props.displayName}
+          variant="beam"
+          colors={["#A7C5BD", "#E5DDCB", "#EB7B59", "#CF4647", "#524656"]}
+        />
+      </div>
+      <div>
+        <span className="text-xl font-bold">{props.displayName}</span>
+        <br />
+        <span className="opacity-60 text-sm"> {props.content}</span>
+      </div>
+    </div>
+  );
+}
+
+export default MessageCard;
