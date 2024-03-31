4e1ad02b7fb320c2de7f656a2878c4878cdb6ea7
Ian Koech <ianovic8@live.com>
2024-03-31 13:18:11 +0300

    Messages Uploaded

---
+++ csm/app/api/messageUpload/route.ts
@@ -0,0 +1,23 @@
+import { addDoc, collection } from "firebase/firestore";
+import { firestore } from "../configs/firebaseconfig";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  try {
+    body.forEach((obj: { [x: string]: any }, index: any) => {
+      const docRef = addDoc(collection(firestore, "messages"), {
+        "User ID": obj["User ID"],
+        "Timestamp (UTC)": obj["Timestamp (UTC)"],
+        "Message Body": obj["Message Body"],
+        Name: obj["Name"],
+        "Message ID": obj["Message ID"],
+      });
+      console.log("Document written");
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+  return new Response("Still Testing", { status: 200 });
+}
