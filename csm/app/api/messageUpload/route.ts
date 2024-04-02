3ba229cc6ec3f211f4410e3150ecae41da0f70fd
Ian Koech <ianovic8@live.com>
2024-04-02 11:26:57 +0300

    Source Data upload Endpoint

--- csm/app/api/messageUpload/route.ts
+++ csm/app/api/messageUpload/route.ts
@@ -3,18 +3,14 @@ import { firestore } from "../configs/firebaseconfig";
 
 export async function POST(request: Request) {
   const body = await request.json();
-
+  // console.log(body.length());
   try {
-    body.forEach((obj: { [x: string]: any }, index: any) => {
-      const docRef = addDoc(collection(firestore, "messages"), {
-        "User ID": obj["User ID"],
-        "Timestamp (UTC)": obj["Timestamp (UTC)"],
-        "Message Body": obj["Message Body"],
-        Name: obj["Name"],
-        "Message ID": obj["Message ID"],
-      });
-      console.log("Document written");
-    });
+    for (const index in body) {
+      const docRef = addDoc(
+        collection(firestore, "archivedConversations"),
+        body[index]
+      );
+    }
   } catch (e) {
     console.error("Error adding document: ", e);
   }
